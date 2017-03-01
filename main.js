c8469649164a0706e3dd9e69fc4dc4a255b1842f
Trenton Pottruff <jayitinc@gmail.com>
2017-03-01 02:16:12 -0500

    First Real Version

---
+++ main.js
@@ -0,0 +1,18 @@
+$('#home-button').click(function() {
+  $('.section').show();
+});
+
+$('#bio-button').click(function() {
+  $('.section').hide();
+  $('#bio').show();
+});
+
+$('#projects-button').click(function() {
+  $('.section').hide();
+  $('#projects').show();
+});
+
+$('#education-button').click(function() {
+  $('.section').hide();
+  $('#education').show();
+});
