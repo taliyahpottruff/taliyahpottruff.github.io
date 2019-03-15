9f8eedb823f38e6afd5f60291b1a0040a0b31a3f
Trenton Pottruff <jayitinc@gmail.com>
2019-03-15 10:19:03 -0400

    Better design

---
+++ code.cs
@@ -0,0 +1,10 @@
+
+
+
+Person GetAbout() {
+    trentonPottruff = new Person();
+    trentonPottruff.softwareEngineer = true;
+    trentonPottruff.entrepeneur = true;
+    trentonPottruff.contentCreator = true;
+    return trentonPottruff;
+}
