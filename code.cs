dd58900566260bf9741fe320133588482529441a
Taliyah Pottruff <tpottruff@gmail.com>
2022-10-21 21:16:11 -0400

    Update for Job Hunting

--- code.cs
+++
@@ -1,8 +0,0 @@
-Person GetAbout() {
-    var taliyahPottruff = new Person();
-    taliyahPottruff.softwareEngineer = true;
-    taliyahPottruff.pilot = true;
-    taliyahPottruff.trans = true;
-    taliyahPottruff.awarenessOfBadJokes = 0.25f;
-    return taliyahPottruff;
-}
